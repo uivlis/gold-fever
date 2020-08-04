5cec5b75a2cade7aea9e8862c594e6bea62027f7
uivlis <vladsilviufarcas@gmail.com>
2020-08-04 08:29:31 +0300

    New draft of TPL

--- smart-contracts/contracts/ThePromisedLand.sol
+++ smart-contracts/contracts/ThePromisedLand.sol
@@ -1,11 +1,10 @@
 pragma solidity ^0.6.0;
 
-import "@openzeppelin/contracts/presets/ERC20PresetMinterPauser.sol";
+import "erc-1155/contracts/ERC1155MixedFungibleMintable.sol";
 
 
 /**
  * @dev Gold Fever - The Promised Land
  */
-contract ThePromisedLand is ERC20PresetMinterPauser {
-    constructor() public ERC20PresetMinterPauser("The Promised Land", "TPL") {}
+contract ThePromisedLand is ERC1155MixedFungibleMintable {
 }
